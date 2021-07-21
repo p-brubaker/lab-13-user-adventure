6fd3dbce9f98b147e37f6e631048ef4303cc8c86
Paul Brubaker <pbrubak@gmail.com>
2021-07-20 17:19:19 -0700

    add results.js

--- results/results.js
+++ results/results.js
@@ -1,3 +1,17 @@
 // Import needed functions
+import { getUser } from '../data/api.js';
+import { getHealthMessage, getTreasureMessage, getResultImage} from './messages.js';
 
 // Grab DOM elements
+const storyDisplay = document.getElementById('story-display');
+const resultImage = document.getElementById('result-image');
+
+const user = getUser();
+
+function renderResult(user) {
+    const healthMessage = getHealthMessage(user.hp);
+    const treasureMessage = getTreasureMessage(user.hp, user.treasure);
+    const img = getResultImage(user.hp, user.treasure);
+}
+
+renderResult(user);
